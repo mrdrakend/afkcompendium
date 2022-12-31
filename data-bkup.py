ddcffe1a47ed6408a55379a75fef6c3b00dd145a
mrdrakend <mrdrakend@gmail.com>
2022-12-31 12:59:54 -0300

    Pyside code conversion & improvement
    v 0.3
    
    Additions
    - New main.py fusing PySide 6. Old main.py will be kept as back-up
    - data.py updated to include 100+ heroes. Old data.py will be kept as back-up
    - "images" folder updated to include facction incons, hero images, and signature items.

---
+++ data-bkup.py
@@ -0,0 +1,78 @@
+# CREATE A DICIONARY WITH THE DATA FROM THE MAIN FILE RELATED TO THE HEROES
+heroes_dict = {
+    'Alna' : {
+        'name': 'Alna',
+        'fac': 'Celestial',
+        'role': 'Debuffer',
+        'img': 'images/heroes/alna.png',
+        'sig_img': 'images/items/alna_sigitem.webp',
+        'sig': 'Frostbite',
+        'skills': {
+            'skill_1': 'Winter War Cry',
+            'skill_2': 'Freezing Pierce',
+            'skill_3': 'Winter\'s Call',
+            'skill_4': 'Frozen Fury'
+        },
+        'background': '#F2E297'
+    },
+    'Arthur' : {
+        'name': 'Arthur',
+        'fac': 'Dimensional',
+        'role': 'Tank',
+        'img': 'images/heroes/arthur.webp',
+        'sig_img': 'images/items/arthur_sigitem.webp',
+        'sig': 'Excalibur',
+        'skills': {
+            'skill_1': 'Smite',
+            'skill_2': 'Excalibur\'s Fury',
+            'skill_3': 'King\'s Blessing',
+            'skill_4': 'Shield of Honnor'
+        },
+        'background': '1F75FE'
+    },
+    'Belinda' : {
+        'name': 'Belinda',
+        'fac': 'Lightbringer',
+        'role': 'DPS',
+        'img': 'images/heroes/belinda.webp',
+        'sig_img': 'images/items/belinda_sigitem.webp',
+        'sig': 'Staff of Light',
+        'skills': {
+            'skill_1': 'Divine Retribution',
+            'skill_2': 'Brillance',
+            'skill_3': 'Blessing',
+            'skill_4': 'Divine Light'
+        },
+        'background': 'FFFFFF'
+    },
+    'Numisu' : {
+        'name': 'Numisu',
+        'fac': 'Mauler',
+        'role': 'Buffer',
+        'img': 'images/heroes/numisu.webp',
+        'sig_img': 'images/items/numisu_sigitem.webp',
+        'sig': 'Mask of Visions',
+        'skills': {
+            'skill_1': 'Resonating Blast',
+            'skill_2': 'Rejuvenating Totem',
+            'skill_3': 'Offensive Totem',
+            'skill_4': 'Fanaticism'
+        },
+        'background': 'FF0000'
+    },
+    'Tasi' : {
+        'name': 'Tasi',
+        'fac': 'Mauler',
+        'role': 'DPS',
+        'img': 'images/heroes/tasi.webp',
+        'sig_img': 'images/items/tasi_sigitem.webp',
+        'sig': 'The Dream Halo',
+        'skills': {
+            'skill_1': 'Slumber',
+            'skill_2': 'Dream Spirit',
+            'skill_3': 'Banishment',
+            'skill_4': 'Teleportation'
+        },
+        'background': '779A71'
+    }
+}
